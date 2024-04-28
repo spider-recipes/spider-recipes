b98a6fa026efde60d02aeda4b986ae62cd7c559c
Bryce <bryce.grahn@bbd.co.za>
2024-04-28 22:35:03 +0200

    Remove server security group

--- terraform/security-group.tf
+++ terraform/security-group.tf
@@ -34,39 +34,6 @@ resource "aws_security_group" "db_security_group" {
 }
 
 # Define security group for the EC2 instance
-resource "aws_security_group" "eb_security_group_server" {
-  vpc_id      = aws_vpc.vpc.id
-  name        = "${var.project_name}-eb-security-group-server"
-  description = "Security group for eb"
-
-  # Define inbound rules to allow traffic from anywhere to the EC2 instance on port 8080 (for Spring Boot)
-  ingress {
-    from_port   = var.eb_port_server
-    to_port     = var.eb_port_server
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Define inbound rules to allow traffic from anywhere to the EC2 instance on port 22 (for SSH)
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Define outbound rules to allow the EC2 instance to communicate with the RDS database
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = merge(var.mandatory_tags, { Name = "${var.project_name}-eb-security-group-server" })
-}
-
-# Define security group for the EC2 instance
 resource "aws_security_group" "eb_security_group_web" {
   vpc_id      = aws_vpc.vpc.id
   name        = "${var.project_name}-eb-security-group-web"
