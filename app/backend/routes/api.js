bb0eb3bb217f9c0c8b25bae61ab50c6940ede468
Bryce <bryce.grahn@bbd.co.za>
2024-05-05 13:05:31 +0200

    Upload images to s3 bucket

--- app/backend/routes/api.js
+++ app/backend/routes/api.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// This is only here as an example 
+
+router.get('/', function (req, res, next) {
+  res.send('Welcome to the server api');
 });
 
 module.exports = router;
