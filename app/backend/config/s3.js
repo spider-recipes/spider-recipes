d2fa4ae1db3053713812eb98c9213bd95f90e3e1
Bryce <bryce.grahn@bbd.co.za>
2024-05-06 12:27:56 +0200

    stuff

--- app/backend/config/s3.js
+++ app/backend/config/s3.js
@@ -2,8 +2,6 @@ require('dotenv').config(); // Load environment variables from .env file
 const aws = require('aws-sdk')
 
 const region = process.env.AWS_REGION
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
 
 // Configure AWS SDK to automatically fetch credentials from EC2 instance metadata
 var creds = new aws.EC2MetadataCredentials();
@@ -12,11 +10,8 @@ if (creds.accessKeyId !== undefined) {
   aws.config.credentials = creds;
 }
 
-
 const s3 = new aws.S3({
   region,
-  // accessKeyId,
-  // secretAccessKey,
   signatureVersion: 'v4'
 })
 
