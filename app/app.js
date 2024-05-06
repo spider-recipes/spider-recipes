e81c12ff36f8aff01e4aecd8f0b75ea0802a2a64
Bryce <bryce.grahn@bbd.co.za>
2024-05-06 08:27:50 +0200

    fix

--- app/app.js
+++ app/app.js
@@ -7,17 +7,6 @@ require('dotenv').config(); // Load environment variables from .env file
 
 const PORT = process.env.PORT || process.env.PORT_LOCAL || 80;
 
-const db_connection_config = {
-  server: process.env.DB_CONNECTION_STRING_NO_PORT,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: 'SpiderRecipes',
-  options: {
-    encrypt: true,
-    trustServerCertificate: true
-  }
-};
-
 const app = express();
 
 if (process.env.MODE === 'debug') {
@@ -47,22 +36,3 @@ app.get("/*", (request, response) => {
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
-
-// async function executeQuery(connection_config, query) {
-//   try {
-
-//     const pool = await sql.connect(connection_config);
-//     const result = await pool.request().query(query);
-
-//     console.log(result.recordset);
-//     await pool.close();
-
-//   } catch (err) {
-//     // Handle errors
-//     console.error('This specific error:', err);
-//   }
-// }
-// query1 = 'SELECT * FROM Recipes';
-// query2 = 'SELECT * FROM Users';
-// executeQuery(db_connection_config, query1);
-// executeQuery(db_connection_config, query2);
