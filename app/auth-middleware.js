9d06be3123100c6d6dc163b49cf5341494bc1f8c
Bryce <157360632+bryce-bbd@users.noreply.github.com>
2024-05-09 17:10:32 +0200

    Update auth-middleware.js

--- app/auth-middleware.js
+++ app/auth-middleware.js
@@ -1,5 +1,6 @@
 const { auth } = require("express-oauth2-jwt-bearer");
 const authConfig = require("./auth_config.json");
+require("dotenv").config();
 
 console.log("apisecret", process.env.APISECRET);
 
